668549b91814b1eb2b204712e9b68d86a29e9675
Sergey Bakaev <sergey.bakaev@hotmail.com>
2019-08-16 23:29:12 +0300

    add base reactjs logic

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,1 +1,9 @@
-export const sum = (a, b) => a + b;
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => <h1>Hello from React JS!</h1>;
+
+render(
+  <App />,
+  document.getElementById('root'),
+);
