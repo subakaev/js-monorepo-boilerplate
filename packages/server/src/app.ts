fb2c2187b1768f57cbe0559b01eecbb858b34ebf
Sergey Bakaev <bakaev@yandex.ru>
2019-08-18 15:03:39 +0300

    husky with lint-staged configure for monorepo

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
 export default (): express.Application => {
